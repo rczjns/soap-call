80011273c84084eea763928bdd91d42ce99f05c9
rczjns <rczjns@gmail.com>
2019-03-03 21:44:54 +0100

    init example

---
+++ hello_client.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'savon'
+
+client = Savon.client(wsdl: 'http://www.learnwebservices.com/services/hello?WSDL')
+response = client.call(
+  :say_hello,
+  soap_action: '',
+  message: { 'HelloRequest' => { 'Name' => 'John Doe' } }
+)
+puts response.body[:say_hello_response][:hello_response][:message]
+# Hello John Doe!
